63d05d223b3f1288edb0c6672f202bdb7e986300
osquaaa <osquaa@gmail.com>
2024-11-30 17:50:58 +0300

    ,,

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const cors = require('cors')
