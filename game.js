28e1574ebdba0b07fcafcb777c509a7c5ffd9eb4
osquaaa <136095047+osquaaa@users.noreply.github.com>
2024-12-03 19:03:33 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,61 +1,65 @@
-let username = localStorage.getItem('username') || 'guest (без аккаунта)';
-let token = localStorage.getItem('token');
-let score = 0;
-let coins = 0;
-let coinsPerClick = 1;
-let multiplier = 1;
+let username = localStorage.getItem('username') || 'guest (без аккаунта)'
+let token = localStorage.getItem('token')
+let score = 0
+let coins = 0
+let coinsPerClick = 1
+let multiplier = 1
 
-const apiUrl = 'https://tapalka-home.onrender.com'; // Ваш адрес на Render
+const apiUrl = 'https://tapalka-home.onrender.com' // Ваш адрес на Render
 
 // Обновление интерфейса
 function updateUI() {
-    const registerForm = document.getElementById('register-form');
-    const loginForm = document.getElementById('login-form');
-    const logoutBtn = document.getElementById('logout-btn');
-    const greeting = document.getElementById('greeting');
-    const scoreElement = document.getElementById('score');
-    const coinsElement = document.getElementById('coins');
-    document.getElementById('upgrade-click').textContent = `+1 К КЛИКУ (${coinsPerClick * 100} монет)`;
-    document.getElementById('upgrade-double').textContent = `ДАБЛ КЛИК (${multiplier * 10000} монет)`;
+	const registerForm = document.getElementById('register-form')
+	const loginForm = document.getElementById('login-form')
+	const logoutBtn = document.getElementById('logout-btn')
+	const greeting = document.getElementById('greeting')
+	const scoreElement = document.getElementById('score')
+	const coinsElement = document.getElementById('coins')
+	document.getElementById('upgrade-click').textContent = `+1 К КЛИКУ (${
+		coinsPerClick * 100
+	} монет)`
+	document.getElementById('upgrade-double').textContent = `ДАБЛ КЛИК (${
+		multiplier * 10000
+	} монет)`
 
-    if (token) {
-        registerForm.style.display = 'none';
-        loginForm.style.display = 'none';
-        logoutBtn.style.display = 'block';
-        greeting.textContent = `Привет, ${username}`;
-    } else {
-        registerForm.style.display = 'block';
-        loginForm.style.display = 'block';
-        logoutBtn.style.display = 'none';
-        greeting.textContent = '';
-    }
+	if (token) {
+		registerForm.style.display = 'none'
+		loginForm.style.display = 'none'
+		logoutBtn.style.display = 'block'
+		greeting.textContent = `Привет, ${username}`
+	} else {
+		registerForm.style.display = 'block'
+		loginForm.style.display = 'block'
+		logoutBtn.style.display = 'none'
+		greeting.textContent = ''
+	}
 
-    scoreElement.textContent = `Очков: ${score}`;
-    coinsElement.textContent = `Монеты: ${coins}`;
+	scoreElement.textContent = `Очков: ${score}`
+	coinsElement.textContent = `Монеты: ${coins}`
 }
 
 // Функция для обновления данных пользователя
 async function fetchUser() {
-    if (!token) return;
+	if (!token) return
 
-    try {
-        const response = await fetch(`${apiUrl}/user/${username}`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        if (!response.ok) throw new Error('Пользователь не найден');
+	try {
+		const response = await fetch(`${apiUrl}/user/${username}`, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		if (!response.ok) throw new Error('Пользователь не найден')
 
-        const user = await response.json();
-        score = user.score;
-        coins = user.coins;
-        coinsPerClick = user.coinsPerClick;
-        multiplier = user.multiplier;
+		const user = await response.json()
+		score = user.score
+		coins = user.coins
+		coinsPerClick = user.coinsPerClick
+		multiplier = user.multiplier
 
-        updateUI();
-    } catch (err) {
-        alert(err.message);
-    }
+		updateUI()
+	} catch (err) {
+		alert(err.message)
+	}
 }
 async function fetchTopUsers() {
 	try {
@@ -104,165 +108,170 @@ fetchTopUsers()
 setInterval(fetchTopUsers, 7000)
 // Функция для клика по монете
 async function clickCoin() {
-    if (!token) return;
+	if (!token) return
 
-    try {
-        const response = await fetch(`${apiUrl}/click/${username}`, {
-            method: 'POST',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        if (!response.ok) throw new Error('Ошибка при обработке клика');
+	try {
+		const response = await fetch(`${apiUrl}/click/${username}`, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		if (!response.ok) throw new Error('Ошибка при обработке клика')
 
-        const user = await response.json();
-        score = user.score;
-        coins = user.coins;
-        updateUI();
-    } catch (err) {
-        alert(err.message);
-    }
+		const { score: updatedScore, coins: updatedCoins } = await response.json()
+		score = updatedScore
+		coins = updatedCoins
+		updateUI()
+	} catch (err) {
+		showError(err.message)
+	}
 }
 
 // Функция для покупки +1 к монетам за клик
 async function buyClickUpgrade() {
-    if (!token) return;
+	if (!token) return
 
-    try {
-        const response = await fetch(`${apiUrl}/upgrade/click/${username}`, {
-            method: 'POST',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        if (!response.ok) {
-            const data = await response.json();
-            throw new Error(data.message || 'Ошибка при покупке улучшения');
-        }
+	try {
+		const response = await fetch(`${apiUrl}/upgrade/click/${username}`, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		if (!response.ok) {
+			const data = await response.json()
+			throw new Error(data.message || 'Ошибка при покупке улучшения')
+		}
 
-        const data = await response.json();
-        alert(data.message);
-        fetchUser();
-    } catch (err) {
-        alert(err.message);
-    }
+		const data = await response.json()
+		alert(data.message)
+		fetchUser()
+	} catch (err) {
+		alert(err.message)
+	}
 }
 
 // Функция для покупки дабл клика
 async function buyDoubleUpgrade() {
-    if (!token) return;
+	if (!token) return
 
-    try {
-        const response = await fetch(`${apiUrl}/upgrade/double/${username}`, {
-            method: 'POST',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        if (!response.ok) {
-            const data = await response.json();
-            throw new Error(data.message || 'Ошибка при покупке улучшения');
-        }
+	try {
+		const response = await fetch(`${apiUrl}/upgrade/double/${username}`, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		if (!response.ok) {
+			const data = await response.json()
+			throw new Error(data.message || 'Ошибка при покупке улучшения')
+		}
 
-        const data = await response.json();
-        alert(data.message);
-        fetchUser();
-    } catch (err) {
-        alert(err.message);
-    }
+		const data = await response.json()
+		alert(data.message)
+		fetchUser()
+	} catch (err) {
+		alert(err.message)
+	}
 }
 
 // Функции для регистрации и авторизации
 async function register() {
-    const usernameInput = document.getElementById('register-username').value;
-    const passwordInput = document.getElementById('register-password').value;
+	const usernameInput = document.getElementById('register-username').value
+	const passwordInput = document.getElementById('register-password').value
 
-    if (!usernameInput || !passwordInput) {
-        alert('Пожалуйста, заполните все поля');
-        return;
-    }
+	if (!usernameInput || !passwordInput) {
+		alert('Пожалуйста, заполните все поля')
+		return
+	}
 
-    try {
-        const response = await fetch(`${apiUrl}/register`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username: usernameInput,
-                password: passwordInput,
-                role: 'user',
-            }),
-        });
+	try {
+		const response = await fetch(`${apiUrl}/register`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				username: usernameInput,
+				password: passwordInput,
+				role: 'user',
+			}),
+		})
 
-        if (!response.ok) throw new Error('Ошибка при регистрации');
+		if (!response.ok) throw new Error('Ошибка при регистрации')
 
-        const data = await response.json();
-        localStorage.setItem('token', data.token);
-        username = usernameInput;
-        localStorage.setItem('username', username);
-        alert('Регистрация успешна!');
-        location.reload();
-    } catch (err) {
-        alert(err.message);
-    }
+		const data = await response.json()
+		localStorage.setItem('token', data.token)
+		username = usernameInput
+		localStorage.setItem('username', username)
+		alert('Регистрация успешна!')
+		location.reload()
+	} catch (err) {
+		alert(err.message)
+	}
 }
 
 async function login() {
-    const usernameInput = document.getElementById('login-username').value;
-    const passwordInput = document.getElementById('login-password').value;
+	const usernameInput = document.getElementById('login-username').value
+	const passwordInput = document.getElementById('login-password').value
 
-    if (!usernameInput || !passwordInput) {
-        alert('Пожалуйста, заполните все поля');
-        return;
-    }
+	if (!usernameInput || !passwordInput) {
+		alert('Пожалуйста, заполните все поля')
+		return
+	}
 
-    try {
-        const response = await fetch(`${apiUrl}/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username: usernameInput,
-                password: passwordInput,
-            }),
-        });
+	try {
+		const response = await fetch(`${apiUrl}/login`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				username: usernameInput,
+				password: passwordInput,
+			}),
+		})
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.message || 'Ошибка авторизации');
-        }
+		if (!response.ok) {
+			const errorData = await response.json()
+			throw new Error(errorData.message || 'Ошибка авторизации')
+		}
 
-        const data = await response.json();
-        localStorage.setItem('token', data.token);
-        localStorage.setItem('username', usernameInput);
-        username = usernameInput;
+		const data = await response.json()
+		localStorage.setItem('token', data.token)
+		localStorage.setItem('username', usernameInput)
+		username = usernameInput
 
-        alert('Авторизация успешна!');
-        location.reload();
-    } catch (err) {
-        alert(err.message);
-    }
+		alert('Авторизация успешна!')
+		location.reload()
+	} catch (err) {
+		alert(err.message)
+	}
 }
 
 // Обработчики для кнопок
-document.getElementById('coin').addEventListener('click', clickCoin);
-document.getElementById('upgrade-click').addEventListener('click', buyClickUpgrade);
-document.getElementById('upgrade-double').addEventListener('click', buyDoubleUpgrade);
+document.getElementById('coin').addEventListener('click', clickCoin)
+document
+	.getElementById('upgrade-click')
+	.addEventListener('click', buyClickUpgrade)
+document
+	.getElementById('upgrade-double')
+	.addEventListener('click', buyDoubleUpgrade)
 document.getElementById('logout-btn').addEventListener('click', function () {
-    localStorage.removeItem('username');
-    localStorage.removeItem('token');
-    location.reload();
-});
+	localStorage.removeItem('username')
+	localStorage.removeItem('token')
+	location.reload()
+})
 
 // Обработчики для регистрации и логина
-document.getElementById('register-btn').addEventListener('click', register);
-document.getElementById('login-btn').addEventListener('click', login);
+document.getElementById('register-btn').addEventListener('click', register)
+document.getElementById('login-btn').addEventListener('click', login)
 
 // Загрузка данных пользователя при входе
 if (token) {
-    fetchUser();
+	fetchUser()
 } else {
-    alert('Пожалуйста, войдите в систему');
+	alert('Пожалуйста, войдите в систему')
 }
+
